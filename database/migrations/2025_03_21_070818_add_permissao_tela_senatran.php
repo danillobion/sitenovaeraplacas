3da69ed29bdeabfe71358a6de4117562c2e5fa97
Danillo Bion <danillobion@gmail.com>
2025-03-21 07:17:46 +0000

    ajuste na permissao do senatran e melhorias na usabilidade

---
+++ database/migrations/2025_03_21_070818_add_permissao_tela_senatran.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::table('permissoes')->insert([
+            ['nome' => 'tela-senatran', 'descricao' => 'Tela para acessar o Senatran', 'created_at' => now(), 'updated_at' => now()],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
